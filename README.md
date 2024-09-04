35ae915d9c1b1a8a97e2ca7dbf035b1dddb76ca5
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-09-04 16:59:35 -0500

    Set up project

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# odin-shopping-cart
 This is a project made with React using react-router and css-modules. It is part of the React Course from The Odin Project.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Shopping Cart</h1>
+      <div className="card"></div>
+    </>
+  );
+}
+
+export default App;
