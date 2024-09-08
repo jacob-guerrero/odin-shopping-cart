b807fbf8924f6ae4873bab4d73e6f02aa538211d
Jacob <104036951+jacob-guerrero@users.noreply.github.com>
2024-09-08 15:33:48 -0500

    Add home component with basic style

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a project made with React using react-router and css-modules. It is part of the React Course from The Odin Project.
+
+Credits:
+Photo by Ismael Abdal Naby studio: https://www.pexels.com/photo/man-in-white-suit-wearing-brown-hat-standing-on-green-grass-field-14544169/
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,29 @@
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <>
-      <h1>Home</h1>
-      <div></div>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroImageContainer}>
+          <img
+            src="src\assets\model1-home.jpg"
+            alt="Man in White Suit Wearing Brown Hat Standing on Green Grass Field"
+            className={styles.heroImage}
+          />
+        </div>
+        <div className={styles.heroTextContainer}>
+          <h1>Experience Your Own Heaven</h1>
+          <p>
+            Step into a world where fashion meets serenity. Exclusive
+            collections designed to elevate your Style. Bring a touch of
+            elegance to every occasion.
+          </p>
+          <Link to="shop">
+            <button>Shop Now</button>
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
